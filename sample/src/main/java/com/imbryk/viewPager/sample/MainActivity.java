b642e7b7f6ac64939aed2726265addf8e86ea80d
広畑 壮一郎 <shirohata@gmail.com>
2015-11-23 15:35:17 +0900

    add sample

---
+++ sample/src/main/java/com/imbryk/viewPager/sample/MainActivity.java
@@ -0,0 +1,47 @@
+package com.imbryk.viewPager.sample;
+
+import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
+        viewPager.setAdapter(new Adapter());
+    }
+
+    private static class Adapter extends PagerAdapter {
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            TextView tv = new TextView(container.getContext());
+            tv.setText(String.valueOf(position));
+            container.addView(tv);
+            return tv;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View) object);
+        }
+
+        @Override
+        public int getCount() {
+            return 4;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+    }
+}
